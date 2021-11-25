ed9b41dde0f1f67b03193301e3c568e0f721eee4
LUIS ANAYA <brothersailorwebdev@gmail.com>
2021-11-25 16:33:32 +0100

    trying to deploy

--- components/destination/Hero.js
+++ components/destination/Hero.js
@@ -8,11 +8,11 @@ export default function Hero({ hero }) {
   return (
     <div className="relative bg-dark pb-32 shadow-xl sm:rounded-2xl sm:overflow-hidden">
       <div className="w-full h-full z-0">
-        {hero.coverImage && (
+        {hero?.coverImage && (
           <>
             <Image
-              src={urlForImage(hero.coverImage).url()}
-              alt={hero.coverImage.alt}
+              src={urlForImage(hero?.coverImage).url()}
+              alt={hero?.coverImage?.alt}
               layout="fill"
               objectFit="cover"
               quality={100}
@@ -25,9 +25,9 @@ export default function Hero({ hero }) {
       <header className="relative py-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="w-full max-w-3xl mx-auto text-center text-white space-y-3">
-            <h1 className="tw-heading z-10">{hero.heading}</h1>
+            <h1 className="tw-heading z-10">{hero?.heading}</h1>
             <div className="text-lg z-10">
-              <TextBody content={hero.body} />
+              <TextBody content={hero?.body} />
             </div>
           </div>
         </div>
