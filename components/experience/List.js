6cc6b51031c0770beb13b39461b2cb54120ca58c
LUIS ANAYA <brothersailorwebdev@gmail.com>
2021-12-23 03:27:36 +0100

    fixed experience padding

--- components/experience/List.js
+++ components/experience/List.js
@@ -1,6 +1,6 @@
 export default function List({ children }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 sm:gap-x-24 gap-y-12 sm:gap-y-24">
+    <div className="grid grid-cols-1 sm:grid-cols-3 sm:gap-x-24 gap-y-12 sm:gap-y-24 px-12 sm:px-24">
       {children}
     </div>
   );
