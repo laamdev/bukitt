d2fa9fd09801c159ff8d134b1126721f8e19aaad
LUIS ANAYA <brothersailorwebdev@gmail.com>
2021-12-27 17:28:33 +0100

    added cache for fonts

--- next.config.js
+++ next.config.js
@@ -12,4 +12,116 @@ module.exports = {
   images: {
     domains: ['assets.vercel.com', 'cdn.sanity.io'],
   },
+  async headers() {
+    return [
+      {
+        source: '/fonts/neue/neuemontreal-thin.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/neue/neuemontreal-lightitalic.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/neue/neuemontreal-regular.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/neue/neuemontreal-italic.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/neue/neuemontreal-medium.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/neue/neuemontreal-mediumitalic.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/neue/neuemontreal-bold.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/neue/neuemontreal-bolditalic.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/stellar/stellar-light.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/stellar/stellar-regular.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/stellar/stellar-medium.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/fonts/stellar/stellar-bold.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+    ];
+  },
 };
