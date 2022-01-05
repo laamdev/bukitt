d0634a5a5ee24461ddd23e30ec2bcf5a811a4d4e
LUIS ANAYA <brothersailorwebdev@gmail.com>
2022-01-05 19:40:24 +0100

    fixing deploy

--- studio/schemas/objects/shared/hero.js
+++ studio/schemas/objects/shared/hero.js
@@ -10,28 +10,10 @@ export default {
       description: 'Hero heading.',
     },
     {
-      name: 'tagline',
-      title: 'Tagline',
-      type: 'string',
-      description: 'Hero tagline.',
-    },
-    {
       name: 'body',
       type: 'text',
       title: 'Body',
       description: 'Hero body.',
-      // // validation: (Rule) => [
-      // //   Rule.required()
-      // //     .min(100)
-      // //     .error('Body is required and must be min. 100 char.'),
-      // //   Rule.max(250).error('Body must be max. 250 char.'),
-      // // ],
-    },
-    {
-      name: 'callToAction',
-      type: 'callToAction',
-      title: 'CTA',
-      description: 'Hero CTA.',
     },
     {
       name: 'coverImage',
