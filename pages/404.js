8b805e0dec15ed7cca771a93bcb1370b6bade289
LUIS ANAYA <brothersailorwebdev@gmail.com>
2022-01-17 05:02:28 +0100

    added 404 custom page

---
+++ pages/404.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+export default function ErrorPage() {
+  return (
+    <main className="flex-grow mx-auto max-w-7xl w-full flex flex-col px-4 sm:px-6 lg:px-8">
+      <div className="flex-shrink-0 my-auto py-16 sm:py-32">
+        <p className="text-sm font-semibold text-brand-600 uppercase tracking-wide font-mono">
+          404 error
+        </p>
+        <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+          Page not found
+        </h1>
+        <p className="mt-2 text-base text-gray-500">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-6">
+          <Link href="/">
+            <a className="text-base font-medium text-brand-600 hover:text-brand-500">
+              Go back home<span aria-hidden="true"> &rarr;</span>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+}
