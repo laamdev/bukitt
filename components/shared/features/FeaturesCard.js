c1e1a8620c71fa2239ecc5843b141d333094d307
LUIS ANAYA <brothersailorwebdev@gmail.com>
2022-01-20 16:51:11 +0100

    fixed icons for features

--- components/shared/features/FeaturesCard.js
+++ components/shared/features/FeaturesCard.js
@@ -8,16 +8,18 @@ export default function FeaturesCard({ feature }) {
       <div className="-mt-6">
         <div>
           <div className="bg-brand-500 rounded-md shadow-lg w-12 h-12 mx-auto p-2">
-            <Image
-              src={urlForImage(feature?.icon).width(1080).height(1080).url()}
-              alt={feature?.icon?.alt}
-              layout="responsive"
-              width={1}
-              height={1}
-              objectFit="cover"
-              objectPosition="center"
-              aria-hidden="true"
-            />
+            {feature?.icon && (
+              <Image
+                src={urlForImage(feature?.icon).width(1080).height(1080).url()}
+                alt={feature?.icon?.alt}
+                layout="responsive"
+                width={1}
+                height={1}
+                objectFit="cover"
+                objectPosition="center"
+                aria-hidden="true"
+              />
+            )}
           </div>
         </div>
         <h3 className="mt-8 text-2xl lg:text-3xl font-mono uppercase font-medium text-neutral-900 tracking-tight">
