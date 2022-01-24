e5d23b2e440c1bab2e0d7c849b35e61439433fed
LUIS ANAYA <brothersailorwebdev@gmail.com>
2022-01-24 18:15:55 +0100

    checking events pixel

--- components/shared/buttons/BtnInquiryForm.js
+++ components/shared/buttons/BtnInquiryForm.js
@@ -1,8 +1,14 @@
-export default function BtnNewsletter({ disabled, btnLinkText }) {
+import * as fbq from '@/lib/fpixel';
+
+export default function BtnInquiryForm({ disabled, btnLinkText }) {
+  const handleClick = () => {
+    fbq.event('trackCustom', 'Inquire');
+  };
   return (
     <button
       type="submit"
       disabled={disabled}
+      onClick={handleClick}
       className="tw-btn tw-btn-transparent-brand disabled:opacity-50 disabled:hover:bg-transparent disabled:hover:text-brand disabled:cursor-not-allowed"
     >
       {btnLinkText}
